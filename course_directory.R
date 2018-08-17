fab043b9f4e5f037697e512fe28e10cd8c791c53
Veronica Armour <vlarmour@gmail.com>
2018-08-17 11:18:25 -0400

    Add files via upload
    Add file for web scraping accordion style course directory across several pages.

---
+++ course_directory.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+library(xml2)
+library(rvest)
+
+sci_courses <- lapply(paste0('https://comminfo.rutgers.edu/academics/courses?courses=&program=All&page=', 0:22),
+                      function(url){
+                        url %>% read_html() %>%
+                          html_nodes(".panel-heading span, #course-num span, .panel-body") %>%
+                          html_text()
+                      })
+
+## renaming columns with generic names
+
+names(sci_courses) <- c("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w")
+
+## this identifies problematic columns
+sci_courses <- as.tibble(sci_courses)
+
+## removing problematic columns
+
+sci_courses$u <- NULL
+sci_courses$v <- NULL
+sci_courses$w <- NULL
+
+## now tibble will work
+sci_courses <- as.tibble(sci_courses)
+
+## export 
+write_csv(sci_courses, "your_file_path_here")
